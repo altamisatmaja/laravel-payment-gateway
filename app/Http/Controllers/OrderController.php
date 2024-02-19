f46b19b5b784d1052d40e719635ffbd5b3a99f10
altamisatmaja <altamisfattah16@gmail.com>
2024-02-19 07:40:58 +0700

    feat: midtrans configuration

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,15 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    public function home() {
+    public function home()
+    {
         return view('home');
     }
 
-    public function checkout(Request $request) {
-        dd($request->all());
+    public function checkout(Request $request)
+    {
+        $request->request->add(['total_price' => $request->qty * 100000, 'status' => 'Unpaid']);
+        $order = Order::create($request->all());
+
+        /*Install Midtrans PHP Library (https://github.com/Midtrans/midtrans-php)
+        composer require midtrans/midtrans-php
+
+        Alternatively, if you are not using **Composer**, you can download midtrans-php library
+        (https://github.com/Midtrans/midtrans-php/archive/master.zip), and then require
+        the file manually.
+
+        require_once dirname(__FILE__) . '/pathofproject/Midtrans.php'; */
+
+        // SAMPLE REQUEST START HERE
+
+        // Set your Merchant Server Key
+        \Midtrans\Config::$serverKey = config('midtrans.server_key');
+        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+        \Midtrans\Config::$isProduction = false;
+        // Set sanitization on (default)
+        \Midtrans\Config::$isSanitized = true;
+        // Set 3DS transaction for credit card to true
+        \Midtrans\Config::$is3ds = true;
+
+        $params = array(
+            'transaction_details' => array(
+                'order_id' => $order->id,
+                'gross_amount' => $order->total_price,
+            ),
+            'customer_details' => array(
+                'first_name' => $order->name,
+                'phone' => $order->phone,
+            ),
+        );
+
+        $snapToken = \Midtrans\Snap::getSnapToken($params);
+        dd($snapToken);
+        return view('/checkout', compact('snapToken', 'order'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -57,32 +57,6 @@
                         </label>
                     </div>
                 </div>
-                {{-- <div class="inline-flex items-center">
-                    <label class="relative -ml-2.5 flex cursor-pointer items-center rounded-full p-3" for="checkbox"
-                        data-ripple-dark="true">
-                        <input type="checkbox"
-                            class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-pink-500 checked:bg-pink-500 checked:before:bg-pink-500 hover:before:opacity-10"
-                            id="checkbox" />
-                        <span
-                            class="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20"
-                                fill="currentColor" stroke="currentColor" stroke-width="1">
-                                <path fill-rule="evenodd"
-                                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                        </span>
-                    </label>
-                    <label class="mt-px cursor-pointer select-none font-light text-gray-700" for="checkbox">
-                        <p
-                            class="flex items-center font-sans text-sm font-normal leading-normal text-gray-700 antialiased">
-                            Saham siap dibayar
-                            <a class="font-semibold transition-colors hover:text-pink-500" href="#">
-                                &nbsp;Tunai
-                            </a>
-                        </p>
-                    </label>
-                </div> --}}
                 <button
                     class="mt-6 block w-full select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                     type="submit" data-ripple-light="true">
