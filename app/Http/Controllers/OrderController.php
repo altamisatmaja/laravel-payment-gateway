54a1167300ab40adf5e0e4a98e5f1b115cc38e8e
altamisatmaja <altamisfattah16@gmail.com>
2024-02-19 07:07:49 +0700

    feat: tailwindcss, views, omigrate order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    public function home() {
+        return view('home');
+    }
+
+    public function checkout(Request $request) {
+        dd($request->all());
+    }
 }
